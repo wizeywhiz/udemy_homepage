960dd15a240c54996cedb3bbc366a311f66b4aab
wizeywhiz <wizeycool@gmail.com>
2023-02-09 02:02:18 +0100

    added pop up info pages for distinct courses

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-
+document.querySelectorAll('.course').forEach(course =>{
+    course.addEventListener('mouseover', e =>{
+        let data = `<h3 class="heading">${course.querySelector('.course-title').textContent}</h3>
+        <h4>And any other data</h4>`;
+        let infoBox = course.querySelector('#course-info');
+        infoBox.innerHTML = data;
+        infoBox.classList.remove('hidden');
+        infoBox.classList.add('pop-up');
+    });
+    course.addEventListener('mouseleave', e =>{
+        let infoBox = course.querySelector('#course-info');
+        infoBox.classList.remove('pop-up');
+        infoBox.classList.add('hidden');
+    })
+})
